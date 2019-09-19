256b1343a5382d8531c135add9d0dd0953671d46
SRBarua <baruasudipta@gmail.com>
2019-09-19 17:25:12 -0600

    fontcolor

--- resources/views/marketing/index.blade.php
+++ resources/views/marketing/index.blade.php
@@ -84,6 +84,20 @@
             </div>
         </section>
 
+        <section id="secTwo">
+            <div class="jumbotron">
+                <div class="container">
+                    <h1 class="display-4">Hello, James!</h1>
+                    <p class="lead">You are the BEST.</p>
+                    <hr class="my-4">
+
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more about James</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
